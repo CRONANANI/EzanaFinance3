567ead89199b3d087174dcba8e2e041a1d4ce550
Wadad Parker <wadadparker5@gmail.com>
2025-09-07 08:11:45 +0530

    feat: react router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,15 @@
+import { Routes , Route } from "react-router"
 
 function App() {
 
   return (
-      <div>
+    <Routes>
+      <Route path="/" element={<div>
         <h1 className=" text-center text-5xl text-red-500">Ezana Finance</h1>
-      </div>
+      </div>} />
+
+    </Routes>
+      
 
   )
 }
