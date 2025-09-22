654615f20011646c1cfaf492fb31d5b490291b53
CRONANANI <noahasheber1@gmail.com>
2025-09-22 13:57:29 -0400

    Fix navigation flow: Make landing.html the main index page with consistent notifications sidebar

--- app/components/navigation/navigation-config.js
+++ app/components/navigation/navigation-config.js
@@ -4,7 +4,7 @@ const NavigationConfig = {
     brand: {
         name: 'Ezana Finance',
         icon: 'bi bi-graph-up',
-        href: '../index.html'
+        href: 'index.html'
     },
 
     // Navigation menu items - matches landing page structure
